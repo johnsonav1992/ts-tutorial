cf719cf3c60c760d6e8b68451261969d8f394d8e
Alex Johnson <johnsonav1992@gmail.com>
2023-07-04 05:43:31 -0500

    init

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log( 'This is a template repo' );
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+
+
+// STATIC TYPE ANNOTATIONS
+
+const numberOfFriends: number = 4;
+const firstName: string = 'Alex';
+let thisStillWorks = true;
