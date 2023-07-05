66f598ce0caea225e3fd413cd78fd7a5fbe785de
Alex Johnson <johnsonav1992@gmail.com>
2023-07-05 06:39:55 -0500

    typos

--- intermediate-01.ts
+++ intermediate-01.ts
@@ -12,7 +12,7 @@
 // ASSERTIONS //
 ////////////////
 
-// Assertions are instances when you (for whatever reason) need to tell TypeScript that you know better about the situation going on and need to essentially override its typechecking with something specific. We have actualy already seen an assertion in our sample program from basics!
+// Assertions are instances when you (for whatever reason) need to tell TypeScript that you know better about the situation going on and need to essentially override its typechecking with something specific. We have actually already seen an assertion in our sample program from basics!
 
 //-- As keyword (typecasting) --//
 // This keyword allows you to typecast, meaning force something to be a type that it may or may not be
@@ -24,7 +24,7 @@ console.log( ( result as string ).length );
 // TS allows this because it is unknown. If it knew it was a number, you would have to first convert it to a string and typecasting would throw an error
 
 //-- NonNull Assertion (!) --//
-// This assertion is used for when you absoutely know something will exist (not be null or undefined) and you are making TS aware of that. Use this with CAUTION! Only when you are 100% (or more) sure that this is the case.
+// This assertion is used for when you absolutely know something will exist (not be null or undefined) and you are making TS aware of that. Use this with CAUTION! Only when you are 100% (or more) sure that this is the case.
 
 interface Data {
     data: string;
